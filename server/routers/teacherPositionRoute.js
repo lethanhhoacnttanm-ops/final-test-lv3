b00d65eee5ee8e650a86f234e7970d46ee45b143
lethanhhoa <lethanhhoa_cntt_anm@gmail.com>
2026-07-07 15:24:13 +0700

    n

--- server/routers/teacherPositionRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import teacherPositionController from "../controller/teacherPositionController.js";
-
-const routerTeacherPosition = Router();
-
-routerTeacherPosition.get("/", teacherPositionController.getTeacherPositions);
-routerTeacherPosition.post("/", teacherPositionController.postTeacherPosition);    
-
-export default routerTeacherPosition;
--- server/routers/teachersRoute.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import teacherController from '../controller/teacherController.js';
-
-const routerTeachers = Router();
-
-routerTeachers.get('/', teacherController.getTeachers);
-routerTeachers.post('/', teacherController.postTeacher);
-
-export default routerTeachers;
